0a5631ebca0fffa1c1716e7a99cfd6b6fd762a01
AleksandrShokk <104966254+AleksandrShokk@users.noreply.github.com>
2023-07-11 12:35:18 +0800

    3

--- src/Components/screens/Home/Beat/Beat.jsx
+++ src/Components/screens/Home/Beat/Beat.jsx
@@ -5,8 +5,10 @@ import styles from './Beat.module.scss'
 const Beat = ({ data }) => {
 	const [cart, setCart] = useLocalStorage('cart', {})
 	const navigate = useNavigate()
+	let counter = 0
 	data?.map(item => {
 		if (item.isBought === true) {
+			counter++
 		}
 	})
 
@@ -19,43 +21,48 @@ const Beat = ({ data }) => {
 					<h3>Цена</h3>
 				</div>
 			</div>
-			{data?.map((item, index) => (
-				<div
-					key={index}
-					className={
-						item.isBought === false
-							? styles.properties
-							: styles.disActive
-					}
-				>
-					<h5>{`${item.name.toUpperCase().split('(')[0]}  (by ${
-						item.author
-					})`}</h5>
-					<audio
-						src={item.pathMp3}
-						controls={true}
-						controlsList='nodownload'
-					></audio>
-					<button
-						id={item.id}
-						onClick={() => {
-							setCart({
-								id: item.id,
-								name: item.name,
-								priceForMP3: item.priceForMP3,
-								priceForWav: item.priceForWav,
-								priceForSoundtracks: item.priceForSoundtracks,
-								pathTrack: item.pathMp3
-							})
-							setTimeout(() => {
-								navigate('/checkout')
-							}, 1)
-						}}
+			{counter === 0 ? (
+				<h3>К сожалению битов пока нет!</h3>
+			) : (
+				data?.map((item, index) => (
+					<div
+						key={index}
+						className={
+							item.isBought === false
+								? styles.properties
+								: styles.disActive
+						}
 					>
-						<Link>{item.priceForMP3}P</Link>
-					</button>
-				</div>
-			))}
+						<h5>{`${item.name.toUpperCase().split('(')[0]}  (by ${
+							item.author
+						})`}</h5>
+						<audio
+							src={item.pathMp3}
+							controls={true}
+							controlsList='nodownload'
+						></audio>
+						<button
+							id={item.id}
+							onClick={() => {
+								setCart({
+									id: item.id,
+									name: item.name,
+									priceForMP3: item.priceForMP3,
+									priceForWav: item.priceForWav,
+									priceForSoundtracks:
+										item.priceForSoundtracks,
+									pathTrack: item.pathMp3
+								})
+								setTimeout(() => {
+									navigate('/checkout')
+								}, 1)
+							}}
+						>
+							<Link>{item.priceForMP3}P</Link>
+						</button>
+					</div>
+				))
+			)}
 		</div>
 	)
 }
